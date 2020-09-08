b166751fb15662d3a41db1bd61e3b6d7a5cffebb
Oscar R <ktaoscar@gmail.com>
2020-09-08 16:12:06 -0600

    segundo commit

---
+++ src/assets/mocks/featured.js
@@ -0,0 +1,32 @@
+import moment from 'moment'
+
+export default [
+    {
+        title: 'Software Engineer Salary in 2020',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Tech Culture'],
+        link: '#',
+        image: 'money.jpg'
+    },
+    {
+        title: 'Software Engineer Salary in 2020',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Javascript'],
+        link: '#',
+        image: 'money.jpg'
+    },
+    {
+        title: 'Software Engineer Salary in 2020',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Tech Culture'],
+        link: '#',
+        image: 'money.jpg'
+    },
+    {
+        title: 'Software Engineer Salary in 2020',
+        date: moment().format('MMMM DD, YYYY'),
+        categories: ['Tech Culture'],
+        link: '#',
+        image: 'money.jpg'
+    }
+]
---
+++ src/components/common/index.js
@@ -0,0 +1,10 @@
+import MasonryPost from './masonry-post'
+import PostMasonry from './post-mansonry'
+import PostGrid from './post-grid'
+import TagRow from './tag-row'
+export {
+    MasonryPost,
+    PostMasonry,
+    PostGrid,
+    TagRow
+}
